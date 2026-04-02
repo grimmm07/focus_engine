991fabbd00688c66ac0ce6c0d9e7b076a7f1b453
grimmm07 <anaschahbalani@gmail.com>
2026-04-02 15:24:42 +0100

    I updated the summary. Now you can visualize everything with graphs, which helps you identify your most productive days and the ones where you struggled to stay focused.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tracker import tracker
+from summary import summary
 print("1 - Start focus tracker ")
-print("2 - Exit")
-data = input ("Can you chose a number from the menu: ")
-
-if data == "1":
-    tracker()
+print("2 - to get a summary ")
+print("3 - Exit")
+data = ""
+while data != '3':
+    data = input ("Can you chose a number from the menu: ")
+    if data == "1":
+        tracker()
+    if data == "2":
+        summary()
 
--- summary.py
+++ summary.py
@@ -1,14 +1,27 @@
 import csv
+import matplotlib.pyplot as plt
 
 def summary():
     duration = []
+    data_by_date = {}
     with open('sessions.csv', mode="r") as f:
         data = csv.reader(f)
         for row in data:
+            test = row[0].split(' ')
+            if test[0] not in data_by_date:
+                data_by_date[test[0]] = 0
+            data_by_date[test[0]] += float(row[2])
             duration.append(float(row[2]))
     if len(duration) == 0:
         print("there is no data for now come later")
         return
+    dates = list(data_by_date.keys())
+    minutes = [seconds / 60 for seconds in data_by_date.values()]
+
+    plt.bar(dates, minutes, color='skyblue', edgecolor='navy')
+    plt.xticks(rotation=45) 
+    plt.show()
+    print(dates)
     print(f"Sessions: {len(duration)}")
     print(f"{'Total focus time ':<20} : {int(sum(duration) // 60)} min {int (sum(duration) % 60)} sec")
     print(f"{'Average session ':<20} : {int((sum(duration) // len(duration)) // 60)} min {int ((sum(duration) // len(duration)) % 60)} sec")
